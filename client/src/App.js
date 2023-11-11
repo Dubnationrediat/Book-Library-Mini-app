c98ecffa07717d8e76335647eed2951dcb90ca3e
Rediat Terefe <rediat_ta@ch.iitr.ac.in>
2023-11-11 23:04:16 +0530

    delete functionality and react routing futures added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import FileUplaod from './pages/FileUpload';
-import Home from './pages/Home';
-
+import FileUplaod from './pages/Uplaod/FileUpload';
+import Home from './pages/Home/Home';
+import { Routes, Route, useNavigate} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      
-   <FileUplaod/>
 
-   <Home/>
+   <Routes>
+    <Route path='/' element={  <Home/>}/>
+    <Route path='/home' element={  <Home/>}/>
+    <Route path='/upload' element={  <FileUplaod/>}/>
+    
+  </Routes>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ import loginRoute from './Routes/loginR.js'
 import insertBooksRoute from './Routes/bookInsertR.js'
 import selectAllRoute from './Routes/getAllBooksR.js'
 import PdfSender from './Routes/dawnload.js'
+import deleteR from './Routes/deleteR.js'
 
 
 
@@ -22,6 +23,7 @@ app.use('/user',loginRoute)
 app.use('/user',insertBooksRoute)
 app.use('/user',selectAllRoute)
 app.use('/user',PdfSender)
+app.use('/user',deleteR)
 
 
 function connectionCreator(){
